d1f994ed55ef4477b7d7123a0b31d48572b1eefe
abhiraj75 <itsabhiraj27@gmail.com>
2026-02-13 22:20:48 +0530

    complete download function

---
+++ youtube.py
@@ -0,0 +1,20 @@
+from pytube import YouTube
+import tkinter as tk
+from tkinter import filedialog
+
+def download_video(url,save_path):
+    try:
+        yt=YouTube(url)
+        streams=yt.streams.filter(progressive=True, file_extension="mp4")
+        highest_res_streams=streams.get_highest_resolution()
+        highest_res_streams.download(output_path=save_path)
+        print("Video Downloaded Successfully!")
+
+    except Exception as e:
+        print(e)
+
+
+url=""
+save_path=""
+
+download_video(url,save_path)
